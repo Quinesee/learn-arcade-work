005a7d410ebeafc3262d4ead280061c6b2cab2ea
Joshua Norvelle <joshua.norvelle@gmail.com>
2022-01-04 15:06:40 -0600

    Lab 01 - Test print()

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-print('hi')
+print("Testing the print() function!")
+print("""This one
+spans multiple lines
+but looks weird in code.""")
+print("So I like to use the \\n to create \nnew lines")
+print("\tThis line is tabbed in!")
